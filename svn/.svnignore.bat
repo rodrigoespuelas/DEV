6bea95675dd52280c15708daf2d0d0f57bd62db1
Rodrigo Espuelas Garmilla <rodrigoespuelas@yahoo.es>
2018-09-13 12:51:12 +0200

    Update .svnignore.bat

--- svn/.svnignore.bat
+++ svn/.svnignore.bat
@@ -1,3 +1,2 @@
 ##Limitations Any previous settings in the global-ignores of each folder will be eliminated by the script.
 cmd /k svn -R propset svn:ignore -F .svnignore .
-##cmd /k svn propset svn:ignore "*.txt *.bak" .
